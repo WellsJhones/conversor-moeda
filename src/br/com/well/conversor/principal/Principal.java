7a1f989e90ad4585ebd79ba45aa6b8ea9d084527
WellsJhones <wellsjhones@gmail.com>
2024-04-25 07:51:05 -0300

    show menu using a loop

--- src/br/com/well/conversor/principal/Principal.java
+++ src/br/com/well/conversor/principal/Principal.java
@@ -1,5 +1,6 @@
 package br.com.well.conversor.principal;
 
+import br.com.well.conversor.principal.module.WriteFiles;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import java.io.IOException;
@@ -7,15 +8,13 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Principal {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner scanner = new Scanner(System.in);
 
-        String endereco = String.format("https://v6.exchangerate-api.com/v6/(your_api_key_here)/latest/USD");
+        String endereco = String.format("https://v6.exchangerate-api.com/v6/(your-key-here)/latest/USD");
 
         HttpClient client = HttpClient.newHttpClient();
 
@@ -32,23 +31,24 @@ public class Principal {
 
         // Access the conversion_rates object
         JsonObject conversionRates = jsonObject.getAsJsonObject("conversion_rates");
-
-        // Define the 10 most used currencies
-        List<String> currencies = Arrays.asList("USD", "EUR", "JPY", "GBP", "AUD", "CAD", "CHF", "CNY", "SEK", "NZD");
+        List<String> currencies = new ArrayList();
 
         // Display the currencies to the user
-        System.out.println("Select two currencies and an amount to convert:");
-        for (int i = 0; i < currencies.size(); i++) {
-            System.out.println((i + 1) + ". " + currencies.get(i));
+        for (int i = 0;  i < 25; i++) {
+            System.out.println(String.format("%d - %s",i+1, conversionRates.keySet().toArray()[i]));
+            currencies.add(conversionRates.keySet().toArray()[i].toString());
+
         }
 
-        // Get the user's choices
         System.out.println("Enter the number of the first currency:");
         int firstCurrencyIndex = scanner.nextInt() - 1;
+
         System.out.println("Enter the number of the second currency:");
         int secondCurrencyIndex = scanner.nextInt() - 1;
+
         System.out.println("Enter the amount to convert:");
         double amount = scanner.nextDouble();
+
 
         // Get the conversion rates
         double firstCurrencyToUsdRate = conversionRates.get(currencies.get(firstCurrencyIndex)).getAsDouble();
@@ -57,6 +57,9 @@ public class Principal {
         // Convert the amount to the second currency
         double amountInSecondCurrency = (amount / firstCurrencyToUsdRate) * usdToSecondCurrencyRate;
 
-        System.out.println(amount + " " + currencies.get(firstCurrencyIndex) + " is equivalent to " + amountInSecondCurrency + " " + currencies.get(secondCurrencyIndex));
+        System.out.println(String.format("amount: %.2f %s is equivalent to %.2f %s", amount, currencies.get(firstCurrencyIndex),
+                amountInSecondCurrency, currencies.get(secondCurrencyIndex)));
+
     }
+
 }
