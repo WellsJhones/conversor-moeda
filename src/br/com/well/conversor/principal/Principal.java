1c5c4ac48061d40993f4261d5bafadbddcf30db2
WellsJhones <wellsjhones@gmail.com>
2024-04-25 03:14:28 -0300

    done

--- src/br/com/well/conversor/principal/Principal.java
+++ src/br/com/well/conversor/principal/Principal.java
@@ -1,20 +1,21 @@
 package br.com.well.conversor.principal;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) throws IOException, InterruptedException {
-        Scanner leitura = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-//        System.out.println("Digite o Cep : ");
-//        var cep = leitura.nextLine();
-
-        String  endereco = String.format("https://v6.exchangerate-api.com/v6/c3fcff854bb89b5eeaf7fd02/latest/USD");
+        String endereco = String.format("https://v6.exchangerate-api.com/v6/(your_api_key_here)/latest/USD");
 
         HttpClient client = HttpClient.newHttpClient();
 
@@ -22,8 +23,40 @@ public class Principal {
                 .uri(URI.create(endereco))
                 .build();
 
-        HttpResponse<String> response= HttpClient.newHttpClient().send(request,HttpResponse.BodyHandlers.ofString());
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
         String json = response.body();
-        System.out.println(json);
+
+        // Parse the JSON response using Gson
+        Gson gson = new Gson();
+        JsonObject jsonObject = gson.fromJson(json, JsonObject.class);
+
+        // Access the conversion_rates object
+        JsonObject conversionRates = jsonObject.getAsJsonObject("conversion_rates");
+
+        // Define the 10 most used currencies
+        List<String> currencies = Arrays.asList("USD", "EUR", "JPY", "GBP", "AUD", "CAD", "CHF", "CNY", "SEK", "NZD");
+
+        // Display the currencies to the user
+        System.out.println("Select two currencies and an amount to convert:");
+        for (int i = 0; i < currencies.size(); i++) {
+            System.out.println((i + 1) + ". " + currencies.get(i));
+        }
+
+        // Get the user's choices
+        System.out.println("Enter the number of the first currency:");
+        int firstCurrencyIndex = scanner.nextInt() - 1;
+        System.out.println("Enter the number of the second currency:");
+        int secondCurrencyIndex = scanner.nextInt() - 1;
+        System.out.println("Enter the amount to convert:");
+        double amount = scanner.nextDouble();
+
+        // Get the conversion rates
+        double firstCurrencyToUsdRate = conversionRates.get(currencies.get(firstCurrencyIndex)).getAsDouble();
+        double usdToSecondCurrencyRate = conversionRates.get(currencies.get(secondCurrencyIndex)).getAsDouble();
+
+        // Convert the amount to the second currency
+        double amountInSecondCurrency = (amount / firstCurrencyToUsdRate) * usdToSecondCurrencyRate;
+
+        System.out.println(amount + " " + currencies.get(firstCurrencyIndex) + " is equivalent to " + amountInSecondCurrency + " " + currencies.get(secondCurrencyIndex));
     }
 }
